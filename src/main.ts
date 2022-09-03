ed0b239a3ec9c06429d80c08ded329d71d2205db
armand <armand.salle@gmail.com>
2022-09-03 12:24:49 +0200

    Add gtag

--- src/main.ts
+++ src/main.ts
@@ -10,3 +10,22 @@ if (currentPath === "/reserver") {
 if (currentPath === "/demande") {
   mountBookingRequest()
 }
+
+const GA_ID = "UA-238028753-1"
+const head = document.getElementsByTagName("head")[0]
+
+function loadGA() {
+  let script = document.createElement("script")
+  script.type = "text/javascript"
+  script.src = "https://www.googletagmanager.com/gtag/js?id=" + GA_ID
+  head.appendChild(script)
+  script = document.createElement("script")
+  script.type = "text/javascript"
+  script.textContent =
+    "window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', '" +
+    GA_ID +
+    "' );"
+  head.appendChild(script)
+}
+
+setTimeout(loadGA, 1000)
